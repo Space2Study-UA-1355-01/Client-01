f4f223fddd420de00cb6e1658affb338e56ed205
Tetiana Fedorkiv <fedorkivtanya@gmail.com>
2025-04-11 16:15:39 +0300

    created a fake test

--- src/tests/unit/components/search-input/SearchInput.spec.jsx
+++ src/tests/unit/components/search-input/SearchInput.spec.jsx
@@ -1,4 +1,12 @@
-// import { vi } from 'vitest'
-// import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { expect } from 'vitest'
 
-// import SearchInput from '~/components/search-input/SearchInput'
+import SearchInput from '~/components/search-input/SearchInput'
+
+describe('SearchInput', () => {
+  it('test', () => {
+    render(<SearchInput />)
+    const SearchIcon = screen.getByTestId('search-icon')
+    expect(SearchIcon).toBeInTheDocument()
+  })
+})
