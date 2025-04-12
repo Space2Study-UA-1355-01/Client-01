a208f6190ded34f18c0a371460fd5ea59153f129
Tetiana Fedorkiv <fedorkivtanya@gmail.com>
2025-04-12 18:47:56 +0300

    Initialized first tests for the component

--- src/tests/unit/components/search-input/SearchInput.spec.jsx
+++ src/tests/unit/components/search-input/SearchInput.spec.jsx
@@ -4,9 +4,27 @@ import { expect } from 'vitest'
 import SearchInput from '~/components/search-input/SearchInput'
 
 describe('SearchInput', () => {
-  it('test', () => {
+  beforeEach(() => {
     render(<SearchInput />)
-    const SearchIcon = screen.getByTestId('search-icon')
-    expect(SearchIcon).toBeInTheDocument()
   })
+
+  // it('test', () => {
+  //   const SearchIcon = screen.getByTestId('search-icon')
+  //   expect(SearchIcon).toBeInTheDocument()
+  // })
+
+  it('should render text correctly', () => {
+    const textFieldElement = screen.getByRole('textbox')
+    expect(textFieldElement).toBeInTheDocument()
+  })
+
+  it('should call setSearch when search icon is clicked', () => {
+    /*  const searchIcon = screen.getByTestId('search-icon')
+   expect setSearch to be called after this event */
+  })
+
+  it('should call setState with empty string when delete icon is clicked', () => {})
+  it('should call setSearch when enter is pressed', () => {})
+  it('should have hidden class if search is empty', () => {})
+  it('should have visible class if search is not empty', () => {})
 })
