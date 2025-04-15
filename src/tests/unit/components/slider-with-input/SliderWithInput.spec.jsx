f766fe1a053dcce484115757191f2b6debe5a297
Tetiana Fedorkiv <fedorkivtanya@gmail.com>
2025-04-15 10:29:07 +0300

    Implemented robust tests for SliderWithInput component

--- src/tests/unit/components/slider-with-input/SliderWithInput.spec.jsx
+++ src/tests/unit/components/slider-with-input/SliderWithInput.spec.jsx
@@ -1,57 +1,100 @@
-import { render, screen } from '@testing-library/react'
-import { describe, expect, it, vi } from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import SliderWithInput from '~/components/slider-with-input/SliderWithInput'
-import userEvent from '@testing-library/user-event'
 
-// interface SliderWithInputProps {
-//     defaultValue: number
-//     title: string
-//     max: number
-//     min: number
-//     onChange: (value: number) => void
-//   }
+const handleChangeMock = vi.fn()
+
+const defaultValue = 50
+const min = 0
+const max = 100
+const title = 'Test'
+
+const delay = 500
 
 describe('SliderWithInput', () => {
-  it('should render correctly', () => {
-    const handleChange = vi.fn()
+  beforeEach(() => {
+    vi.useFakeTimers()
     render(
       <SliderWithInput
-        defaultValue={50}
-        max={100}
-        min={0}
-        onChange={handleChange}
-        title='Volume'
+        defaultValue={defaultValue}
+        max={max}
+        min={min}
+        onChange={handleChangeMock}
+        title={title}
       />
     )
-    const titleElement = screen.getByText('Volume')
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should render correctly', () => {
+    const titleElement = screen.getByText('Test')
+
     expect(titleElement).toBeInTheDocument()
   })
 
-  it('should call onChange when slider is moved', async () => {
-    const user = userEvent.setup()
-    const handleChange = vi.fn()
-
-    render(
-      <SliderWithInput
-        defaultValue={50}
-        max={100}
-        min={0}
-        onChange={handleChange}
-        title='Volume'
-      />
-    )
-
-    /*
-    mock a onChange fn -> find user-event for a slider move 
-    */
-
+  it('should call onChange when slider is moved', () => {
     const slider = screen.getByRole('slider')
+    const inputValue = 100
 
-    await user.click(slider)
-    expect(slider).toHaveFocus()
+    fireEvent.change(slider, { target: { value: inputValue } })
+    vi.advanceTimersByTime(delay)
+
+    expect(handleChangeMock).toHaveBeenCalledWith(inputValue)
   })
 
-  it('should update inputValue correctly when input value is empty', () => {})
-  it('should not update prices when input is blurred and value in input has not changed', () => {})
-  it('should update prices when input is blurred and input is greater than max value', () => {})
+  it('should sync slider and input values', () => {
+    const slider = screen.getByRole('slider')
+    const input = screen.getByRole('textbox')
+    const inputValue = 75
+
+    fireEvent.change(slider, { target: { value: inputValue } })
+
+    expect(input).toHaveValue(inputValue.toString())
+
+    fireEvent.change(input, { target: { value: '30' } })
+
+    expect(slider).toHaveValue('30')
+  })
+
+  it('should update inputValue correctly when input value is empty', () => {
+    const input = screen.getByRole('textbox')
+    const inputValue = ''
+
+    fireEvent.change(input, { target: { value: inputValue } })
+
+    expect(input).toHaveValue(inputValue)
+  })
+
+  it('should not update prices when input is blurred and value in input has not changed', () => {
+    const input = screen.getByRole('textbox')
+    const inputValue = min.toString()
+
+    fireEvent.change(input, { target: { value: inputValue } })
+    fireEvent.blur(input)
+
+    expect(input).toHaveValue(inputValue)
+  })
+
+  it('should update prices when input is blurred and input is greater than max value', () => {
+    const input = screen.getByRole('textbox')
+    const inputValue = 101
+
+    fireEvent.change(input, { target: { value: inputValue } })
+    fireEvent.blur(input)
+
+    expect(input).toHaveValue(max.toString())
+  })
+
+  it('should update prices when input is blurred and input is less than min value', () => {
+    const input = screen.getByRole('textbox')
+    const inputValue = -1
+
+    fireEvent.change(input, { target: { value: inputValue } })
+    fireEvent.blur(input)
+
+    expect(input).toHaveValue(min.toString())
+  })
 })
