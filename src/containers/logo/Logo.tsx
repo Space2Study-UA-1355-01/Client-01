bceb1a227f927aedfd4da0817b7f96ddc5206b0f
ssashayurchenko <160735952+ssashayurchenko@users.noreply.github.com>
2025-04-15 10:25:29 +0300

    Close: #229 Fixed logo navigation (#2)
    * Fix logo button navigation issue
    
    * Use data-route for Welcome component binding
    
    * Format code
    
    * Replace scroll logic with HashLink for logo navigation

--- src/containers/logo/Logo.tsx
+++ src/containers/logo/Logo.tsx
@@ -3,18 +3,25 @@ import logo from '~/assets/logo.svg'
 import logoLight from '~/assets/logo-light.svg'
 import Box, { BoxProps } from '@mui/material/Box'
 import { ComponentEnum } from '~/types'
+import HashLink from '~/components/hash-link/HashLink'
+import { guestRoutes } from '~/router/constants/guestRoutes'
 
 interface LogoProps extends BoxProps {
   light?: boolean
 }
 
-const Logo: FC<LogoProps> = ({ light = false, ...props }) => (
-  <Box
-    alt='logo'
-    component={ComponentEnum.Img}
-    src={light ? logoLight : logo}
-    {...props}
-  />
-)
+const Logo: FC<LogoProps> = ({ light = false, ...props }) => {
+  return (
+    <HashLink to={`#${guestRoutes.welcome.route}`}>
+      <Box
+        alt='logo'
+        component={ComponentEnum.Img}
+        src={light ? logoLight : logo}
+        sx={{ cursor: 'pointer' }}
+        {...props}
+      />
+    </HashLink>
+  )
+}
 
 export default Logo
