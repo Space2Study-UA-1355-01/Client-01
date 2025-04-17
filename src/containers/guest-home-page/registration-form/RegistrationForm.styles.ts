7e4a755b620812b3627648d944a6a538e87c2b8c
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2025-04-17 23:26:47 +0300

    Feature/70 pop up with sign up (#22)
    * new branch, no pull request
    
    * 1
    
    * fix
    
    * add registration form popup without validation
    
    * fix bugs in comments
    
    * resolved conflict
    
    * resolved conflict translation

---
+++ src/containers/guest-home-page/registration-form/RegistrationForm.styles.ts
@@ -0,0 +1,27 @@
+import { scrollbar } from '~/styles/app-theme/custom-scrollbar'
+
+export const styles = {
+  form: {
+    overflow: 'auto',
+    maxWidth: { xs: '370px' },
+    minWidth: { md: '340px' },
+    pt: '16px',
+    pr: { md: '0p' },
+    pb: { sm: '0px' },
+    display: 'flex',
+    flexDirection: 'column',
+    ...scrollbar
+  },
+  fullname: {
+    display: 'flex',
+    flexDirection: { xs: 'column', lg: 'row' },
+    gap: { lg: '16px' }
+  },
+  input: {
+    maxWidth: '343px'
+  },
+  loginButton: {
+    width: '100%',
+    py: '14px'
+  }
+}
