3c936e616aaa8acad674ffbf667d2fdc7977fe91
pavlo1377 <pavlo.antohiv@gmail.com>
2025-05-01 19:46:46 +0300

    pageWrapper props changed: added maxWidth prop to make component more flexible

--- src/components/page-wrapper/PageWrapper.tsx
+++ src/components/page-wrapper/PageWrapper.tsx
@@ -7,7 +7,7 @@ import { styles } from '~/components/page-wrapper/PageWrapper.styles'
 import { useModalContext } from '~/context/modal-context'
 
 const PageWrapper = (
-  { children, sx, ...rest }: ContainerProps,
+  { children, sx, maxWidth, ...rest }: ContainerProps,
   ref: Ref<HTMLDivElement>
 ) => {
   const { closeModal } = useModalContext()
@@ -18,8 +18,7 @@ const PageWrapper = (
 
   return (
     <Container
-    // подумати як це зробити reusable типу 
-      // maxWidth='xl'
+      maxWidth={maxWidth}
       ref={ref}
       sx={spliceSx(styles.container, sx)}
       {...rest}
