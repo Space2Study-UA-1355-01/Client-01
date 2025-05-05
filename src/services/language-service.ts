00186ccd434256be6bb0eafe50a9babf29f99372
rudi <yrudikov@gmail.com>
2025-05-05 11:22:12 +0200

    Updated language-service

--- src/services/language-service.ts
+++ src/services/language-service.ts
@@ -1,11 +1,31 @@
 import i18n from '~/plugins/i18n'
 
+const LANGUAGE_KEY = 'app_language'
+
 const languageService = {
   toggleLanguage: () => {
     const nextLang = i18n.language === 'en' ? 'ua' : 'en'
-    void i18n.changeLanguage(nextLang)
+    void languageService.setLanguage(nextLang)
   },
-  getCurrentLanguage: () => i18n.language
+  setLanguage: async (lng: string) => {
+    localStorage.setItem(LANGUAGE_KEY, lng)
+    await i18n.changeLanguage(lng)
+  },
+
+  getSavedLanguage: (): string | null => {
+    return localStorage.getItem(LANGUAGE_KEY)
+  },
+
+  getCurrentLanguage: (): string => {
+    return i18n.language
+  },
+
+  initLanguage: () => {
+    const savedLang = languageService.getSavedLanguage()
+    if (savedLang && i18n.language !== savedLang) {
+      void i18n.changeLanguage(savedLang)
+    }
+  }
 }
 
 export default languageService
