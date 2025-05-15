fe8a5033e46b019f17a6a312d148efa85cace48b
Oleksandra <ssasha.yurchenko@gmail.com>
2025-05-15 11:20:23 +0300

    Added dynamic calculation of items per page based on window width

--- src/containers/find-offer/offer-cards-list/OfferCardsList.tsx
+++ src/containers/find-offer/offer-cards-list/OfferCardsList.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react'
 import { Grid, Box, Stack, Pagination } from '@mui/material'
 
 import usePagination from '~/hooks/table/use-pagination'
+import { itemsLoadLimit } from '~/constants/index'
 
 import OfferCard from '~/components/offer-card/OfferCard'
 import { styles } from '~/containers/find-offer/offer-cards-list/OfferCardsList.styles'
@@ -11,10 +13,29 @@ interface ViewSwitcherProps {
 }
 
 const OfferCardsList = ({ isGridView }: ViewSwitcherProps) => {
-  const mockOffers = Array.from({ length: 7 })
+  const mockOffers = Array.from({ length: 13 })
+
+  const getItemsPerPage = () => {
+    const width = window.innerWidth
+    if (width < 600) return itemsLoadLimit.mobile
+    if (width < 900) return itemsLoadLimit.tablet
+    return itemsLoadLimit.default
+  }
+
+  const [itemsPerPage, setItemsPerPage] = useState(getItemsPerPage)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setItemsPerPage(getItemsPerPage())
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   const { page, rowsPerPage, pageCount, handleChangePage } = usePagination({
     itemsCount: mockOffers.length,
-    itemsPerPage: 6,
+    itemsPerPage,
     defaultPage: 1
   })
 
